aac2ccd01a5bc8e67e74e0730cad314466fa9b89
Loic Deridder <lderidde@fu-r1-p4.s19.be>
2025-03-11 09:13:18 +0100

    dir

---
+++ includes/Vec.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Vec.h                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lderidde <lderidde@student.s19.be>        +#+  +:+       +#+         */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/11 09:07:53 by lderidde          #+#    #+#             */
+/*   Updated: 2025/03/11 09:07:53 by lderidde         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef VEC_H
+# define VEC_H
+
+# include <math.h>
+# include <stdlib.h>
+
+typedef struct s_v3
+{
+	float	x;
+	float	y;
+	float	z;
+}	t_v3;
+
+t_v3	cross(t_v3 a, t_v3 b);
+t_v3	norm(t_v3 vec);
+t_v3	vec_add(t_v3 v1, t_v3 v2);
+t_v3	vec_scale(t_v3 vec, float scale);
+
+#endif
