53bafb7bfb4c89f15edcd478b9dc5d09917e5ec8
Loic Deridder <lderidde@fu-r1-p4.s19.be>
2025-03-24 14:35:00 +0100

    shadows fix

--- srcs/hook/hit.c
+++ srcs/hook/hit.c
@@ -30,7 +30,7 @@ static bool	hit_sp(t_v3 ray, t_sp *sp, t_v3 cam_pos, t_li *li)
 	float	llen;
 
 	llen = len(vec_sub(li->pos, cam_pos));
-	cam_pos = vec_add(cam_pos, ray);
+	cam_pos = vec_add(cam_pos, vec_scale(ray, 0.01));
 	oc = vec_sub(cam_pos, sp->pos);
 	p.a = dot(ray, ray);
 	p.b = 2.0f * dot(oc, ray);
@@ -54,7 +54,7 @@ static bool	hit_pl(t_v3 ray, t_pl *pl, t_v3	cam_pos, t_li * li)
 	float	dist;
 	float	llen;
 
-	cam_pos = vec_add(cam_pos, ray);
+	cam_pos = vec_add(cam_pos, vec_scale(ray, 0.01));
 	llen = len(vec_sub(li->pos, cam_pos));
 	if (dot(ray, pl->norm) == 0.0f)
 		return (false);
@@ -73,7 +73,7 @@ static bool	hit_cl(t_v3 ray, t_cl *cl, t_v3 cam_pos)
 	t_v3	oc_perp;
 	float	pt;
 
-	cam_pos = vec_add(cam_pos, ray);
+	cam_pos = vec_add(cam_pos, vec_scale(ray, 0.01));
 	oc = vec_sub(cam_pos, cl->pos);
 	r_perp = vec_sub(ray, vec_scale(cl->norm, dot(ray, cl->norm)));
 	oc_perp = vec_sub(oc, vec_scale(cl->norm, dot(oc, cl->norm)));
@@ -94,6 +94,29 @@ static bool	hit_cl(t_v3 ray, t_cl *cl, t_v3 cam_pos)
 		   cl->norm);
 		return (fabsf(pt) <= cl->h / 2.0f);
 	}
+}
+
+static bool	hit_cn(t_v3 ray, t_cn *cn, t_v3 cam_pos, t_li *li)
+{
+	t_poly	p;
+	t_v3	oc;
+	float	llen;
+
+	cam_pos = vec_add(cam_pos, vec_scale(ray, 0.01));
+	llen = len(vec_sub(li->pos, cam_pos));
+	oc = vec_sub(cam_pos, cn->pos);
+	p.a = powf(dot(ray, cn->norm), 2) - powf(cos(M_PI / 9), 2);
+	p.b = 2.0 * (dot(ray, cn->norm) * dot(oc, cn->norm) - powf(cos(M_PI / 9), 2) * dot(ray, oc));
+	p.c = powf(dot(oc, cn->norm), 2) - powf(cos(M_PI / 9), 2) * dot(oc, oc);
+	p.delta = p.b*p.b - 4.0 * p.a * p.c;
+	if (p.delta < 0)
+		return (false);
+	p.sol = fminpos((-p.b + sqrt(p.delta)) / (2.0f * p.a), (-p.b - sqrt(p.delta)) / (2.0f * p.a));
+	if (p.sol <= 0)
+		return (false);
+	if (p.sol < llen)
+		return (true);
+	return (false);
 }
 
 bool	hit_sh(t_v3 ray, t_sc *sc, t_v3 pos, t_sh **sh)
@@ -120,6 +143,12 @@ bool	hit_sh(t_v3 ray, t_sc *sc, t_v3 pos, t_sh **sh)
 			return (true);
 		}
 		else if (sc->elems[i].type == CYLINDER && hit_cl(ray, sc->elems[i].sh.cl, pos))
+		{
+			if (sh)
+				*sh = &sc->elems[i].sh;
+			return (true);
+		}
+		else if (sc->elems[i].type == CONE && hit_cn(ray, sc->elems[i].sh.cn, pos, li))
 		{
 			if (sh)
 				*sh = &sc->elems[i].sh;
